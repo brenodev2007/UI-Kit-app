85ecdedea8d18855456be28f09530a9de9e3c407
Breno Soriani <breno.soriani@gmail.com>
2025-08-28 15:59:48 -0300

    Configurando o react-router

--- UiKit-app/src/App.tsx
+++ UiKit-app/src/App.tsx
@@ -1,8 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello World!</h1>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* Aqui você pode adicionar outras rotas depois */}
+      </Routes>
+    </Router>
   );
 }
 
