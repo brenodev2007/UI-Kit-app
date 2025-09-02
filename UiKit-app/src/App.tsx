82b40d1c6f8c114fffc1888bcc86134f358b37cb
Breno Soriani <breno.soriani@gmail.com>
2025-09-02 15:48:16 -0300

    importanto a rota

--- UiKit-app/src/App.tsx
+++ UiKit-app/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import ButtonsPage from "./pages/buttonsPage";
 import CardsPage from "./pages/cardsPage";
+import ExtrasPage from "./pages/extrasPage";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/button" element={<ButtonsPage />} />
         <Route path="/cards" element={<CardsPage />} />
+        <Route path="/extras" element={<ExtrasPage />} />
       </Routes>
     </Router>
   );
---
+++ UiKit-app/src/pages/extrasPage.tsx
@@ -0,0 +1,3 @@
+export default function extrasPage() {
+  return <div>Extras Page</div>;
+}
